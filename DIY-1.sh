f59ec48ec7839f15ce225752453706ea9d9e4f4f
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-19 14:33:47 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -16,14 +16,18 @@ git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci
 # 5-添加 PassWall 插件
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 6-添加外部软件源
 echo "src-git small https://github.com/zouchanggan/openwrt-packages.git;small" >> "feeds.conf.default"
-rm -rf feeds/luci/applications/luci-app-mosdns
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 7-删除依赖(防止插件冲突，删除重复)
 rm -rf feeds/packages/net/{lucky,alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# 删除软件包
+# 删除软件包(防止插件冲突，删除重复)
+rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-vssr
@@ -32,5 +36,4 @@ rm -rf feeds/luci/applications/luci-app-appfilter
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-lucky
 rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/packages/net/smartdns
 ./scripts/feeds update -a && ./scripts/feeds install -a
