d0d2deac6fcbd127de43b980c0998001a3b8e6a9
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-22 10:35:41 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,4 +1,4 @@
-# DIY-2 此脚本功能：修改固件参数
+# 修改固件参数
 # ============================================================================================
 
 # 1-修改管理地址
@@ -23,3 +23,47 @@ sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./inc
 # 7-只显示CPU型号
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 
+# 添加外部插件
+# =======================================================================================================================
+
+# 1-添加 ShadowSocksR Plus+ 插件
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# 2-添加 PowerOff 关机插件
+git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+
+# 3-添加 opentomcat 主题
+git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
+
+# 4-添加 OpenClash 插件
+# sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+
+# 5-添加 PassWall 插件
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 6-添加外部软件源
+echo "src-git small https://github.com/zouchanggan/openwrt-packages.git;small" >> "feeds.conf.default"
+
+# 7-删除依赖(防止插件冲突，删除重复)
+rm -rf feeds/packages/net/{lucky,alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+
+# 8-更新替换golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 9-删除软件包(防止插件冲突，删除重复)
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-vssr
+rm -rf feeds/luci/applications/luci-app-ddns-go
+rm -rf feeds/luci/applications/luci-app-appfilter
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-lucky
+rm -rf feeds/luci/applications/luci-app-smartdns
+
+# 10-更新feeds 
+./scripts/feeds update -a && ./scripts/feeds install -a
