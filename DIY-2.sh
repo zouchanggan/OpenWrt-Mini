e6f64d91a9da64d65224b8d8adee473e3b530640
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-22 13:53:44 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -35,25 +35,28 @@ git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app
 # 3-添加 opentomcat 主题
 git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
-# 4-添加 OpenClash 插件
+# 4-添加 luci-theme-kucat 主题
+git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
+
+# 5-添加 OpenClash 插件
 # sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
-# 5-添加 PassWall 插件
+# 6-添加 PassWall 插件
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
-# 6-添加外部软件源
+# 7-添加外部软件源
 # echo "src-git small https://github.com/zouchanggan/openwrt-packages.git;small" >> "feeds.conf.default"
 
-# 7-删除依赖(防止插件冲突，删除重复)
+# 8-删除依赖(防止插件冲突，删除重复)
 rm -rf feeds/packages/net/{lucky,alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns,dae,daed}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 
-# 8-更新替换golang
+# 9-更新替换golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# 9-删除软件包(防止插件冲突，删除重复)
+# 10-删除软件包(防止插件冲突，删除重复)
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
