395e61cc3929efab835d4f0bf6cfa391edf72762
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2024-12-22 14:17:45 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,4 +1,5 @@
 # DIY-1 此脚本功能：添加外部插件
 # =======================================================================================================================
 # 添加外部软件源
+# git clone -b openwrt https://github.com/zouchanggan/openwrt-packages ./package/small
 echo "src-git small https://github.com/zouchanggan/openwrt-packages.git;openwrt" >> "feeds.conf.default"
